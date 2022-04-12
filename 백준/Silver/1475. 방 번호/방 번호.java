e5ef04c3954e9837d2c67b8ccbfb593ced2208fa
parkchanbin54 <82207081+parkchanbin54@users.noreply.github.com>
2022-04-12 21:05:13 +0900

    [Silver V] Title: 방 번호, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+ 
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        String str = br.readLine();
+ 
+        int size = str.length();
+        int[] arr = new int[10];
+        int temp = 0;
+        int max = 0;
+
+        for (int i = 0; i < size; i++) {
+            temp = str.charAt(i) - '0';
+            arr[temp]++;
+ 
+        }
+
+        int k = (arr[6] + arr[9]);
+
+        if (k % 2 == 0) {
+            arr[6] = k / 2;
+            arr[9] = k / 2;
+        }
+
+        else {
+            arr[6] = k / 2 + 1;
+            arr[9] = k / 2 + 1;
+        }
+
+        for (int i : arr) {
+            max = Math.max(max, i);
+        }
+        System.out.println(max);
+    }
+ 
+}

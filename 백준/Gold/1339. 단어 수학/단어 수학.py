681bd36f758843632fc8936dab72882326e29389
parkchanbin54 <82207081+parkchanbin54@users.noreply.github.com>
2022-07-27 08:29:46 +0900

    [Gold IV] Title: 단어 수학, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+alpha = [] 
+alpha_dict = {}
+numList = [] 
+
+for i in range(n):
+    alpha.append(sys.stdin.readline().rstrip())
+
+for i in range(n): 
+    for j in range(len(alpha[i])):
+        if alpha[i][j] in alpha_dict: 
+            alpha_dict[alpha[i][j]] += 10 ** (len(alpha[i])-j-1)
+        else:  
+            alpha_dict[alpha[i][j]] = 10 ** (len(alpha[i])-j-1)
+
+for val in alpha_dict.values(): 
+    numList.append(val)
+
+numList.sort(reverse=True)
+
+sum = 0
+pows = 9
+for i in numList: 
+    sum += pows * i
+    pows -= 1
+
+print(sum)

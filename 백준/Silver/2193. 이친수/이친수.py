f6dc46482c8e818e1f0a21a122f373ec619d89d9
parkchanbin54 <82207081+parkchanbin54@users.noreply.github.com>
2022-09-07 09:34:35 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,18 @@
+import sys
+
+def sol():
+    input = sys.stdin.readline
+    n = int(input())
+
+    dp = [[0] for _ in range(91)]
+    dp[1] = 1
+    dp[2] = 1
+    dp[3] = 2
+
+    for i in range(4,91):
+        dp[i] = dp[i-2] + dp[i-1]
+
+    print(dp[n])
+
+if __name__ == '__main__':
+    sol()

30ff495d0915031db93a8245b7dd0e70691c31e3
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-02-08 13:38:54 +0900

    [Silver II] Title: 병사 배치하기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -2,14 +2,27 @@ import sys
 
 if __name__ == '__main__':
     input = sys.stdin.readline
+
     n = int(input())
+
     sol = list(map(int,input().split()))
 
-    dp = [1] * n
+    mem = [1e9]
 
-    for i in range(n):
-        for j in range(i):
-            if sol[i] < sol[j]:
-                dp[i] = max(dp[i], dp[j]+ 1)
-    
-    print(len(sol) - max(dp))
+    for s in sol:
+        if mem[-1] > s:
+            mem.append(s)
+        else:
+            left = 0
+            right = len(mem)
+
+            while left < right:
+                mid = (left + right)//2
+
+                if mem[mid] > s:
+                    left = mid + 1
+                else:
+                    right = mid
+            mem[right] = s
+
+    print(n- len(mem)+1)

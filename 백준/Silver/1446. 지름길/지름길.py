f727030f0be2898cceb7119d9781ee04da8d4cae
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-02-08 09:14:42 +0900

    [Silver I] Title: 지름길, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,10 +1,20 @@
-N, D = map(int, input().split())
-li = [list(map(int, input().split())) for _ in range(N)]
-dis = [i for i in range(D+1)]
-for i in range(D+1):
-    if i > 0:
-        dis[i] = min(dis[i], dis[i-1]+1)
-    for s, e, d in li:
-        if i == s and e <= D and dis[i]+d < dis[e]:
-            dis[e] = dis[i]+d
-print(dis[D])
+import sys
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+
+    n, d = map(int,input().split())
+
+    li = [list(map(int,input().split())) for _ in range(n)]
+
+    dp = [i for i in range(d + 1)]
+
+    for i in range(d):
+        if dp[i] + 1 < dp[i+1]:
+            dp[i+1] = dp[i] + 1
+
+        for s, e, c in li:
+            if e <= d:
+                if dp[e] > dp[s] + c:
+                    dp[e] = dp[s] + c
+    print(dp[d])

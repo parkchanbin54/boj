7536e80f1c1a48ba159b7dbb68c0cb191432d1f3
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-02-13 09:46:35 +0900

    [Gold III] Title: 과제, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.py
+++ 백준/Gold/13904. 과제/과제.py
@@ -4,26 +4,25 @@ if __name__ == '__main__':
     input = sys.stdin.readline
 
     n = int(input())
-
     graph = []
     for _ in range(n):
         a,b = map(int,input().split())
         graph.append((a,b))
 
-    graph.sort(key=lambda x: -x[1])
-
     visit = [False for _ in range(1001)]
+
+    graph.sort(key = lambda x : -x[1])
+
     ans = 0
+    for days, worth in graph:
+        i = days
+        while i > 0 and visit[i]:
+            i -= 1
 
-    for time, worth in graph:
-        day = time
-
-        while day > 0 and visit[day]:
-            day -= 1
-
-        if day == 0:
+        if i == 0:
             continue
         else:
-            visit[day] = True
+            visit[i] = True
             ans += worth
+
     print(ans)

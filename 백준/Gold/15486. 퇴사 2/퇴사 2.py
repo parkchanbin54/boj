376080476cf529603d3fe5ba310f4acea09c4bc7
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-02-14 11:04:34 +0900

    [Gold V] Title: 퇴사 2, Time: 2680 ms, Memory: 168896 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -3,13 +3,18 @@ if __name__ == '__main__':
     input = sys.stdin.readline
 
     n = int(input())
-
-    graph = [list(map(int,input().split())) for _ in range(n)]
-    dp = [0 for _ in range(n+1)]
+    t = []
+    b = []
+    for _ in range(n):
+        ti, be = map(int,input().split())
+        t.append(ti)
+        b.append(be)
     m = 0
+
+    dp = [0 for _ in range(n+1)]
     for i in range(n):
         m = max(m, dp[i])
-        if i + graph[i][0] <= n:
-            dp[i + graph[i][0]] = max(graph[i][1] + m, dp[i + graph[i][0]])
+        if i + t[i] <= n:
+            dp[i+t[i]] = max(dp[i+t[i]] , b[i] + m)
 
     print(max(dp))

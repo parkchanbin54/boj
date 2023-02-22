dc05f47094ca18083d42bbbf6d79cf8d4b919e3e
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-02-22 13:37:06 +0900

    [Gold IV] Title: 네트워크 연결, Time: 288 ms, Memory: 54084 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,26 +1,24 @@
 import sys
 
+
 if __name__ == '__main__':
     input = sys.stdin.readline
 
-    N = int(input())
-    M = int(input())
+    n = int(input())
+    m = int(input())
 
-    vRoot = [i for i in range(N+1)]
+    vRoot = [i for i in range(n+1)]
+    graph = [list(map(int,input().split())) for _ in range(m)]
 
-    Elist = [list(map(int,input().split())) for _ in range(M)]
-
-    Elist.sort(key = lambda x:x[2])
-
+    graph.sort(key = lambda x: x[2])
     def find(x):
         if x != vRoot[x]:
             vRoot[x] = find(vRoot[x])
         return vRoot[x]
 
-
     ans = 0
-
-    for s,e,w in Elist:
+    li = []
+    for s,e,w in graph:
         sRoot = find(s)
         eRoot = find(e)
 
@@ -29,8 +27,6 @@ if __name__ == '__main__':
                 vRoot[sRoot] = eRoot
             else:
                 vRoot[eRoot] = sRoot
-
             ans += w
-
 
     print(ans)

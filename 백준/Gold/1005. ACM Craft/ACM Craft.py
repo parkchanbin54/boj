bde4fc02e3affbfb03ae6911dafe5f84b99cfbf4
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-02-27 14:34:08 +0900

    [Gold III] Title: ACM Craft, Time: 1348 ms, Memory: 34464 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -5,37 +5,31 @@ if __name__ == '__main__':
     input = sys.stdin.readline
 
     t = int(input())
-
     for _ in range(t):
         n, k = map(int,input().split())
-        time = [0]+ list(map(int,input().split()))
+        times = [0] + list(map(int,input().split()))
 
         graph = [[] for _ in range(n+1)]
         degree = [0 for _ in range(n+1)]
         dp = [0 for _ in range(n+1)]
-
         for _ in range(k):
             a, b = map(int,input().split())
             graph[a].append(b)
             degree[b] += 1
-
-
-        w = int(input())
 
         queue = deque()
 
         for i in range(1,n+1):
             if degree[i] == 0:
                 queue.append(i)
-                dp[i] = time[i]
-
+                dp[i] = times[i]
 
         while queue:
             tmp = queue.popleft()
             for i in graph[tmp]:
                 degree[i] -= 1
-                dp[i] = max(dp[tmp] + time[i], dp[i])
+                dp[i] = max(dp[tmp] + times[i], dp[i])
                 if degree[i] == 0:
                     queue.append(i)
-
+        w = int(input())
         print(dp[w])

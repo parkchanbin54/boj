6f9412aa02f14aa8bb51c7bdc77fa24d3b09faf3
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-02-28 08:57:57 +0900

    [Gold III] Title: Strahler 순서, Time: 64 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/9470. Strahler 순서/Strahler 순서.py
+++ 백준/Gold/9470. Strahler 순서/Strahler 순서.py
@@ -5,25 +5,27 @@ if __name__ == '__main__':
     input = sys.stdin.readline
 
     t = int(input())
-
     for _ in range(t):
         k,m,p = map(int,input().split())
 
         graph = [[] for _ in range(m+1)]
         degree = [0 for _ in range(m+1)]
         count = [[0,0] for _ in range(m+1)]
-        dp = [0 for _ in range(m+1)]
+        dp = [ 0 for _ in range(m+1)]
+
         for _ in range(p):
-            a,b = map(int,input().split())
+            a, b = map(int,input().split())
             graph[a].append(b)
             degree[b] += 1
 
-
         queue = deque()
+
         for i in range(1,m+1):
             if degree[i] == 0:
                 queue.append(i)
+                dp[i] = i
                 count[i] = [1,1]
+
         while queue:
             now = queue.popleft()
             dp[now] = count[now][0]
@@ -32,11 +34,10 @@ if __name__ == '__main__':
 
             for next in graph[now]:
                 degree[next] -= 1
-                if count[next][0] == dp[now]:
+                if dp[now] == count[next][0]:
                     count[next][1] += 1
-                elif count[next][0] < dp[now]:
+                elif dp[now] > count[next][0]:
                     count[next] = [dp[now], 1]
-
                 if degree[next] == 0:
                     queue.append(next)
 

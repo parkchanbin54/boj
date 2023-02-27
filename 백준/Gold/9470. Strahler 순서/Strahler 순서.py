5c10096e84719603bc7b9b0789f911be95a54d9f
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-02-28 08:51:11 +0900

    [Gold III] Title: Strahler 순서, Time: 64 ms, Memory: 34200 KB -BaekjoonHub

---
+++ 백준/Gold/9470. Strahler 순서/Strahler 순서.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+
+    t = int(input())
+
+    for _ in range(t):
+        k,m,p = map(int,input().split())
+
+        graph = [[] for _ in range(m+1)]
+        degree = [0 for _ in range(m+1)]
+        count = [[0,0] for _ in range(m+1)]
+        dp = [0 for _ in range(m+1)]
+        for _ in range(p):
+            a,b = map(int,input().split())
+            graph[a].append(b)
+            degree[b] += 1
+
+
+        queue = deque()
+        for i in range(1,m+1):
+            if degree[i] == 0:
+                queue.append(i)
+                count[i] = [1,1]
+        while queue:
+            now = queue.popleft()
+            dp[now] = count[now][0]
+            if count[now][1] >= 2:
+                dp[now] += 1
+
+            for next in graph[now]:
+                degree[next] -= 1
+                if count[next][0] == dp[now]:
+                    count[next][1] += 1
+                elif count[next][0] < dp[now]:
+                    count[next] = [dp[now], 1]
+
+                if degree[next] == 0:
+                    queue.append(next)
+
+        print(str(k)+' '+str(dp[m]))

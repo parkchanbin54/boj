c9e956080d54f58861a0b40461fd9a9fa7e45f26
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-02 14:00:37 +0900

    [Silver I] Title: 스타트링크, Time: 788 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -4,24 +4,29 @@ from collections import deque
 if __name__ == '__main__':
     input = sys.stdin.readline
 
-    F,S,G,U,D = map(int,input().split())
-    checked = [1e9] * (F+1)
-    cur = S
-    count = 0
-    move = [U,-D]
+    f,s,g,u,d = map(int,input().split())
+
+    ans = []
     queue = deque()
-    queue.append(cur)
-    checked[cur] = 0
+    queue.append((s,0))
+    flag = False
+
+    visited = [False] * (f + 1)
     while queue:
-        cur = queue.popleft()
-        for i in range(2):
-            ncur = cur + move[i]
+        cur, cnt = queue.popleft()
+        if visited[cur]:
+            continue
+        visited[cur] = True
 
-            if 0 < ncur < (F + 1) and checked[ncur] == 1e9:
-                queue.append(ncur)
-                checked[ncur] = checked[cur] + 1
+        if cur == g:
+            print(cnt)
+            flag = True
+            break
 
-    if checked[G] == 1e9:
+        if cur + u <= f:
+                queue.append((cur + u,cnt+1))
+        if cur - d > 0:
+                queue.append((cur - d,cnt+1))
+
+    if not flag:
         print("use the stairs")
-    else:
-        print(checked[G])

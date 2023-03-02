a3310bdc82e673e779b6e89e09e265c2cfeb4003
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-02 14:26:35 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 196 ms, Memory: 43464 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,29 +1,30 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
+if __name__ == '__main__':
+    input = sys.stdin.readline
 
-n, m = map(int,input().split())
+    n, m = map(int,input().split())
 
+    visited = [[-1,0] for _ in range(100001)]
 
-queue = deque()
-queue.append((n))
-visited = [[-1, 0] for _ in range(100001)]
+    visited[n][0] = 0
+    visited[n][1] = 1
 
-visited[n][0] = 0
-visited[n][1] = 1
+    queue = deque()
+    queue.append(n)
 
-while queue:
-    x = queue.popleft()
+    while queue:
+        cur = queue.popleft()
 
-    for i in [x-1, x+1, x*2]:
-        if 0 <= i <= 100000:
-            if visited[i][0] == -1:
-                visited[i][0] = visited[x][0] + 1
-                visited[i][1] = visited[x][1]
-                queue.append(i)
-            elif visited[i][0] == visited[x][0] + 1:
-                visited[i][1] += visited[x][1]
+        for i in [cur-1, cur+1, cur*2]:
+            if 0 <= i <= 100000:
+                if visited[i][0] == -1:
+                    visited[i][0] = visited[cur][0] + 1
+                    visited[i][1] = visited[cur][1]
+                    queue.append(i)
+                elif visited[i][0] == visited[cur][0] + 1:
+                    visited[i][1] += visited[cur][1]
 
-print(visited[m][0])
-print(visited[m][1])
+    print(visited[m][0])
+    print(visited[m][1])

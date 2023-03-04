0074f75836151fbaba49abdb054e9e64688d754b
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-05 08:34:43 +0900

    [Gold IV] Title: 구슬 찾기, Time: 244 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
@@ -4,26 +4,27 @@ if __name__ == '__main__':
     input = sys.stdin.readline
 
     n, m = map(int,input().split())
-    graph = [[0] * n for _ in range(n)]
-    graph2 = [[0] * n for _ in range(n)]
+
+    graph_up = [[0]* n for _ in range(n)]
+    graph_down = [[0]*n for _ in range(n)]
+
     for _ in range(m):
         a, b = map(int,input().split())
-        graph[a-1][b-1] = 1
-        graph2[b-1][a-1] = 1
-
+        graph_up[a-1][b-1] = 1
+        graph_down[b-1][a-1] = 1
 
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                if graph[i][k] and graph[k][j]:
-                    graph[i][j] = 1
-                if graph2[i][k] and graph2[k][j]:
-                    graph2[i][j] = 1
+                if graph_up[i][k] and graph_up[k][j]:
+                    graph_up[i][j] = 1
+                if graph_down[i][k] and graph_down[k][j]:
+                    graph_down[i][j] = 1
+
     ans = 0
-    for i in range(n):
-        if graph[i].count(0) < n//2 + 1:
-            ans += 1
-        elif graph2[i].count(0) < n//2 + 1:
+
+    for j in range(n):
+        if graph_up[j].count(1) > n//2 or graph_down[j].count(1) > n//2:
             ans += 1
 
     print(ans)

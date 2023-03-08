510d86f327811af0584f69e1ccb35c015fd55c17
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-08 15:18:28 +0900

    [Gold III] Title: 종이 조각, Time: 436 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/14391. 종이 조각/종이 조각.py
@@ -0,0 +1,43 @@
+import sys
+
+def bitmask():
+    global maxans
+
+    for i in range(1 << n * m):
+        total = 0
+        for row in range(n):
+            rowsum = 0
+            for col in range(m):
+                idx = row * m + col
+                if i & (1 << idx) != 0:
+                    rowsum = rowsum * 10 + graph[row][col]
+                else:
+                    total += rowsum
+                    rowsum = 0
+            total += rowsum
+
+        for col in range(m):
+            colsum = 0
+            for row in range(n):
+                idx = row * m +col
+                if i & (1 << idx) == 0:
+                    colsum = colsum * 10 + graph[row][col]
+                else:
+                    total += colsum
+                    colsum = 0
+            total += colsum
+
+        maxans = max(maxans,total)
+
+
+
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+
+    n, m = map(int,input().split())
+    maxans = 0
+    graph = [list(map(int,input().rstrip())) for _ in range(n)]
+
+    bitmask()
+    print(maxans)

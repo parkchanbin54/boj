21dd1450842c9a01e107b8728cb2eeed0050256c
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-12 19:04:52 +0900

    [Gold IV] Title: 소수 경로, Time: 148 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Gold/1963. 소수 경로/소수 경로.py
+++ 백준/Gold/1963. 소수 경로/소수 경로.py
@@ -1,39 +1,48 @@
 import sys
 from collections import deque
+def isprime():
+    for i in range(2,100):
+        if prime[i]:
+            for j in range(i+i, 10000, i):
+                prime[j] = False
+
+def sol(c):
+    queue = deque()
+    queue.append((c,0))
+    flag = False
+    while queue:
+        cur, d = queue.popleft()
+        if cur == b:
+            flag = True
+            break
+
+        for i in range(4):
+            for j in range(10):
+                if i == 0 and j == 0:
+                    continue
+                tmp = int(cur[:i] + str(j) + cur[i+1:])
+                if prime[tmp] and not visited[tmp]:
+                    visited[tmp] = True
+                    queue.append((str(tmp), d+1))
+    if flag:
+        print(d)
+    else:
+        print(0)
+
+
+
+
+
 
 if __name__ == '__main__':
     input = sys.stdin.readline
 
     n = int(input())
-    isprime = [False,False] + [True] * 9999
 
-    for i in range(2,101):
-        if isprime[i]:
-            for j in range(i+i, 10001, i):
-                isprime[j] = False
+    prime = [False,False] + [True] * 9999
+    isprime()
 
     for _ in range(n):
-        a, b = input().split()
-        queue = deque()
-        queue.append((a,0))
-        visited = [False] * 10001
-        flag = False
-        while queue:
-            cur, d = queue.popleft()
-
-            if cur == b:
-                flag = True
-                break
-
-            for i in range(4):
-                for j in range(10):
-                    if i == 0 and j ==0:
-                        continue
-                    tmp = int(cur[:i]+str(j)+cur[i+1:])
-                    if isprime[tmp] and not visited[tmp]:
-                        visited[tmp] = True
-                        queue.append((str(tmp),d+1))
-        if flag:
-            print(d)
-        else:
-            print(0)
+        visited = [False for _ in range(10000)]
+        a,b = input().rstrip().split()
+        sol(a)

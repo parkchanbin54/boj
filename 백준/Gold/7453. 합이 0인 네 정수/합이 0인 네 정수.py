4636fd95568f6b09e5869fcb63bc3ca553203efd
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-14 08:44:21 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 6780 ms, Memory: 664640 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -2,33 +2,37 @@ import sys
 
 if __name__ == '__main__':
     input = sys.stdin.readline
+
     n = int(input())
+
     graph = [list(map(int,input().split())) for _ in range(n)]
-    ab, cd = [], []
+    ab = []
+    cd = []
+
     for i in range(n):
         for j in range(n):
             ab.append(graph[i][0] + graph[j][1])
             cd.append(graph[i][2] + graph[j][3])
-
     ab.sort()
     cd.sort()
 
-    i, j = 0, len(cd) - 1
-    result = 0
-    while i < len(ab) and j >= 0:
-        if ab[i] + cd[j] == 0:
-            nexti, nextj = i + 1, j - 1
+    left = 0
+    right = len(cd) -1
+    cnt = 0
+    while left < len(ab) and right >= 0:
+        tmp = ab[left] + cd[right]
+        if tmp == 0:
+            nleft, nright = left + 1, right - 1
+            while nleft < len(ab) and ab[nleft] == ab[left]:
+                nleft += 1
+            while nright >= 0 and cd[nright] == cd[right]:
+                nright -= 1
+            cnt += (nleft - left) * (right - nright)
+            left, right = nleft , nright
 
-            while nexti < len(ab) and ab[nexti] == ab[i]:
-                nexti += 1
-            while nextj >= 0 and cd[nextj] == cd[j]:
-                nextj -= 1
-
-            result += (nexti - i) * (j - nextj)
-            i, j = nexti, nextj
-        elif ab[i] + cd[j] > 0:
-            j -= 1
+        elif tmp > 0:
+            right -= 1
         else:
-            i += 1
+            left += 1
 
-    print(result)
+    print(cnt)

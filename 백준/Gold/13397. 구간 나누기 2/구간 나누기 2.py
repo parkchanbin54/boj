68ccdb473a5990e63b08de8bb8c42a2f7a1c8a7c
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-14 11:26:11 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.py
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.py
@@ -1,30 +1,30 @@
 import sys
-
-
 def divide(x):
     cnt = 1
-    min_x = nums[0]
-    max_x = nums[0]
+    min_x, max_x = nums[0],nums[0]
+
     for i in range(1,n):
-        min_x = min(nums[i],min_x)
-        max_x = max(nums[i],max_x)
+        min_x = min(min_x, nums[i])
+        max_x = max(max_x, nums[i])
+
         if max_x - min_x > x:
             cnt += 1
             min_x = nums[i]
             max_x = nums[i]
-
     return cnt
 
 if __name__ == '__main__':
     input = sys.stdin.readline
 
-    n, m = map(int, input().split())
+    n, m = map(int,input().split())
     nums = list(map(int,input().split()))
+
     left = 0
     right = max(nums)
     ans = 0
     while left <= right:
         mid = (left + right)//2
+
         if divide(mid) > m:
             left = mid + 1
         else:

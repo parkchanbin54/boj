02ab832b58e5e368c2cca7d51478b284dc2d83b7
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-14 12:00:35 +0900

    [Gold IV] Title: 합이 0, Time: 7052 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.py
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -1,23 +1,23 @@
 import sys
 from collections import Counter
+
+
 def sum3():
     cnt = 0
-
     for i in range(n):
-        l, r = i+1, n-1
+        l, r = i+1 , n-1
         while l < r:
-            tmp = nums[i]+nums[l]+nums[r]
+            tmp = nums[i] + nums[l] + nums[r]
             if tmp == 0:
-                if nums[l] != nums[r]:
-                    cnt += counter[nums[r]]
-                else:
+                if nums[l] == nums[r]:
                     cnt += r - l
+                else:
+                    cnt += counter[nums[r]]
                 l += 1
-            elif tmp > 0:
-                r -= 1
+            elif tmp < 0:
+                l += 1
             else:
-                l += 1
-
+                r -= 1
     return cnt
 
 if __name__ == '__main__':
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     n = int(input())
     nums = list(map(int,input().split()))
-
     nums.sort()
     counter = Counter(nums)
+
     print(sum3())

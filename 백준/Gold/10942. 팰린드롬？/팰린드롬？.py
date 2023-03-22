61dd7beeb97794297bd5fbbbd9c8ea7ab6d3e46e
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-22 17:41:57 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2264 ms, Memory: 61956 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -4,22 +4,21 @@ if __name__ == '__main__':
     input = sys.stdin.readline
 
     n = int(input())
+
     nums = list(map(int,input().split()))
     dp = [[0] * n for _ in range(n)]
+    dp[0][0] = 1
+    for i in range(1,n):
+        for j in range(i+1):
+            if i == j:
+                dp[j][i] = 1
+            elif nums[i] == nums[j]:
+                if j+1 == i: dp[j][i] = 1
+                elif dp[j+1][i-1] == 1:
+                    dp[j][i] = 1
 
-    m = int(input())
+    t = int(input())
 
-    for l in range(n):
-        for s in range(n - l):
-            e = s + l
-
-            if s == e:
-                dp[s][e] = 1
-            elif nums[s] == nums[e]:
-                if s + 1 == e: dp[s][e] = 1
-                elif dp[s+1][e-1] == 1: dp[s][e] = 1
-
-    for q in range(m):
-        s, e = map(int,input().split())
-        print(dp[s-1][e-1])
-
+    for _ in range(t):
+        a,b = map(int,input().split())
+        print(dp[a-1][b-1])

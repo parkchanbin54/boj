bedc9597f76ad7ed16fb95dfe57070122f940983
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-03-22 20:06:59 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 5480 ms, Memory: 314196 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -3,28 +3,27 @@ sys.setrecursionlimit(10**9)
 
 def dfs(idx):
     visited[idx] = True
-    dp[idx][1] = 1
     dp[idx][0] = 0
+    dp[idx][1] = 1
 
     for i in graph[idx]:
         if not visited[i]:
             dfs(i)
             dp[idx][0] += dp[i][1]
-            dp[idx][1] += min(dp[i][0], dp[i][1])
-
+            dp[idx][1] += min(dp[i][1], dp[i][0])
+            
 if __name__ == '__main__':
     input = sys.stdin.readline
 
     n = int(input())
-    graph =[[] * (n+1) for _ in range(n+1)]
+    graph = [[] * (n+1) for _ in range(n+1)]
+    visited = [False] * (n+1)
     dp = [[0,0] for _ in range(n+1)]
     for _ in range(n-1):
-        a,b = map(int,input().split())
+        a, b = map(int,input().split())
         graph[a].append(b)
         graph[b].append(a)
 
-    visited = [False] * (n+1)
-
     dfs(1)
-    print(min(dp[1][1],dp[1][0]))
 
+    print(min(dp[1][0], dp[1][1]))

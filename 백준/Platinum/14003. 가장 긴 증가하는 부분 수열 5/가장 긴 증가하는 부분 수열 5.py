44f08450f50d986912ae326678054e19e48ac098
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-04-14 09:33:44 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 4016 ms, Memory: 218544 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -1,18 +1,15 @@
 import sys
-from collections import defaultdict
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input = sys.stdin.readline
     n = int(input())
-    nums = [0]+ list(map(int,input().split()))
-
-    mem = [-1]
+    nums = list(map(int,input().split()))
     dp = [0] * (n+1)
-    for i in range(1,n+1):
-        if nums[i] > mem[-1]:
-            mem.append(nums[i])
+    mem = [-1000000001]
+    for i,num in enumerate(nums):
+        if mem[-1] < num:
+            mem.append(num)
             dp[i] = len(mem) - 1
-
         else:
             left = 0
             right = len(mem)
@@ -20,19 +17,20 @@ if __name__ == '__main__':
             while left < right:
                 mid = (left + right)//2
 
-                if mem[mid] < nums[i]:
+                if mem[mid] < num:
                     left = mid + 1
                 else:
                     right = mid
             dp[i] = right
             mem[right] = nums[i]
     m = max(dp)
-    ans = []
+    print(m)
+    answer = []
 
-    for i in range(n, 0, -1):
+    for i in range(n-1,-1,-1):
         if dp[i] == m:
-            ans.append(nums[i])
+            answer.append(nums[i])
             m -= 1
 
-    print(len(ans))
-    print(*ans[::-1])
+
+    print(' '.join(map(str,answer[::-1])))

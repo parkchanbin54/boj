55c54b5fd86da9864e928014b0d792fe1564cb9a
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-04-21 15:23:33 +0900

    [Silver I] Title: 금민수의 개수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.py
@@ -0,0 +1,18 @@
+import sys
+from itertools import product
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+x = len(str(a))
+y = len(str(b))
+
+cnt = 0
+
+for i in range(x, y+1):
+    lst = list(product([4, 7], repeat=i))
+    for num in lst:
+        n = int(''.join(map(str, num)))
+        if a <= n <= b:
+            cnt += 1
+
+print(cnt)

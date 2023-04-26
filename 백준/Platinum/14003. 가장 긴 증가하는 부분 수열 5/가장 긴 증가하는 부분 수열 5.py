7b0e2f431a96e7b1a504b782024886b013f8b5a7
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-04-26 09:40:56 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 3944 ms, Memory: 218544 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -2,14 +2,18 @@ import sys
 
 if __name__ == "__main__":
     input = sys.stdin.readline
+
     n = int(input())
     nums = list(map(int,input().split()))
-    dp = [0] * (n+1)
+
     mem = [-1000000001]
-    for i,num in enumerate(nums):
+    dp = [0] * (n+1)
+
+    for i, num in enumerate(nums):
         if mem[-1] < num:
             mem.append(num)
             dp[i] = len(mem) - 1
+
         else:
             left = 0
             right = len(mem)
@@ -21,16 +25,19 @@ if __name__ == "__main__":
                     left = mid + 1
                 else:
                     right = mid
+
             dp[i] = right
-            mem[right] = nums[i]
-    m = max(dp)
-    print(m)
-    answer = []
+            mem[right] = num
 
+    print(len(mem) - 1)
+    k = max(dp)
+
+    arr = []
     for i in range(n-1,-1,-1):
-        if dp[i] == m:
-            answer.append(nums[i])
-            m -= 1
+        if dp[i] == k:
+            arr.append(nums[i])
+            k -= 1
+
+    print(' '.join(map(str,arr[::-1])))
 
 
-    print(' '.join(map(str,answer[::-1])))

afc5d23e370be0ef08db7a3c90f8a5b7544b3a1f
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-05-12 14:28:01 +0900

    [Gold III] Title: 줄 세우기, Time: 200 ms, Memory: 40712 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -5,9 +5,8 @@ if __name__ == '__main__':
     input = sys.stdin.readline
 
     n, m = map(int,input().split())
-
-    graph = [[] for _ in range(n+1)]
     degree = [0 for _ in range(n+1)]
+    graph = [[] for _ in range(n+1)]
 
     for _ in range(m):
         a, b = map(int,input().split())
@@ -15,18 +14,18 @@ if __name__ == '__main__':
         degree[b] += 1
 
     queue = deque()
-
     for i in range(1,n+1):
         if degree[i] == 0:
             queue.append(i)
+
     ans = []
+
     while queue:
-        tmp = queue.popleft()
-        ans.append(tmp)
+        q = queue.popleft()
+        ans.append(q)
+        for x in graph[q]:
+            degree[x] -= 1
+            if degree[x] == 0:
+                queue.append(x)
 
-        for k in graph[tmp]:
-            degree[k] -= 1
-            if degree[k] == 0:
-                queue.append(k)
-
-    print(*ans)
+    print(' '.join(map(str,ans)))

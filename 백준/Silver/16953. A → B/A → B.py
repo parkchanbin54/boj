e5e040ba6f448b0327b4ca00814ec3b3b1cd2365
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-05-12 16:06:55 +0900

    [Silver II] Title: A → B, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,18 +1,27 @@
-from collections import deque
+import sys
 
-a, b = map(int, input().split())
-q = deque()
-q.append((a, 1))
-r = 0
+def dfs(a,l):
+    global answer
+    if a == b:
+        answer = min(answer, l)
+        return
+    if a > b:
+        return -1
+    else:
+        dfs(a*2, l+1)
+        dfs(a*10+1, l+1)
 
-while (q):
-    n, t = q.popleft()
-    if n > b:
-        continue
-    if n == b:
-        print(t)
-        break
-    q.append((int(str(n) + "1"), t + 1))
-    q.append((n * 2, t + 1))
-else:
-    print(-1)
+
+
+
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    answer = 1e9
+    a, b = map(int,input().split())
+
+    dfs(a,0)
+    if answer == 1e9:
+        print(-1)
+    else:
+        print(answer+1)

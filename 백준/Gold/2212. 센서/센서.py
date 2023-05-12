9d6628ef0d6277649c2c53ccc62ba2f9d25879a8
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-05-12 13:57:43 +0900

    [Gold V] Title: 센서, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -5,22 +5,21 @@ if __name__ == '__main__':
 
     n = int(input())
     k = int(input())
-    sensors = list(map(int,input().split()))
-    sensors.sort()
-    
+
     if k >= n:
         print(0)
         sys.exit()
-    
+
+    sensors = list(map(int,input().split()))
+    sensors.sort()
     dis = []
+
     for i in range(1,n):
         dis.append((sensors[i] - sensors[i-1]))
 
-    
-    dis.sort(reverse= True)
+    dis.sort()
 
-    for i in range(k-1):
-        dis.pop(0)
+    for _ in range(k-1):
+        dis.pop()
 
     print(sum(dis))
-

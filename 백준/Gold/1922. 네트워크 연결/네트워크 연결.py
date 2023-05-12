086fe7c0d5e6d53d14b6159d1b07a49ff2ba6a62
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-05-12 14:07:22 +0900

    [Gold IV] Title: 네트워크 연결, Time: 276 ms, Memory: 54084 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,32 +1,30 @@
 import sys
 
-
 if __name__ == '__main__':
     input = sys.stdin.readline
 
     n = int(input())
     m = int(input())
-
     vRoot = [i for i in range(n+1)]
     graph = [list(map(int,input().split())) for _ in range(m)]
-
-    graph.sort(key = lambda x: x[2])
+    graph.sort(key = lambda x : x[2])
     def find(x):
         if x != vRoot[x]:
             vRoot[x] = find(vRoot[x])
         return vRoot[x]
 
-    ans = 0
-    li = []
-    for s,e,w in graph:
-        sRoot = find(s)
-        eRoot = find(e)
+    answer = 0
 
-        if sRoot != eRoot:
-            if sRoot > eRoot:
-                vRoot[sRoot] = eRoot
+    for a,b,w in graph:
+        aRoot = find(a)
+        bRoot = find(b)
+
+        if aRoot != bRoot:
+            if aRoot > bRoot:
+                vRoot[aRoot] = bRoot
             else:
-                vRoot[eRoot] = sRoot
-            ans += w
+                vRoot[bRoot] = aRoot
 
-    print(ans)
+            answer += w
+
+    print(answer)

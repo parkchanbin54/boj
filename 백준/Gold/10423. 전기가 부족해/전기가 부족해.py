ca1481b43a5a6caeb61753fcbfea465f92b88cb4
Park Chanbin <82207081+parkchanbin54@users.noreply.github.com>
2023-05-12 14:14:02 +0900

    [Gold II] Title: 전기가 부족해, Time: 272 ms, Memory: 50012 KB -BaekjoonHub

--- 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
@@ -1,34 +1,42 @@
 import sys
+import heapq
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input = sys.stdin.readline
 
     n, m, k = map(int,input().split())
-    graph = [[] for _ in range(n+1)]
     vRoot = [i for i in range(n+1)]
     state = list(map(int,input().split()))
 
     for s in state:
         vRoot[s] = 0
 
-    li = [list(map(int,input().split())) for _ in range(m)]
-    li.sort(key = lambda x : x[2])
+    graph = []
+
+    for _ in range(m):
+        a, b, c= map(int,input().split())
+        graph.append((a,b,c))
+    graph.sort(key = lambda x : x[2])
     def find(x):
         if x != vRoot[x]:
             vRoot[x] = find(vRoot[x])
         return vRoot[x]
-
     ans = 0
+    for i in range(m):
+        a, b, w = graph[i]
 
-    for a, b, w in li:
         aRoot = find(a)
         bRoot = find(b)
+
         if aRoot != bRoot:
-            ans += w
             if aRoot > bRoot:
                 vRoot[aRoot] = bRoot
             else:
                 vRoot[bRoot] = aRoot
 
+            ans += w
 
     print(ans)
+
+
+
